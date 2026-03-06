798b462a27d72a1a7a3bc53fe6f7a005d85177b9
he-end <hendri41234@gmail.com>
2026-03-07 01:09:28 +0700

    fixup! refactor: implement tree and fix middleware auto corelation that stacked and duplicated activation

--- routes/mw.go
+++ routes/mw.go
@@ -2,9 +2,6 @@ package routes
 
 import (
 	"net/http"
-
-	"github.com/he-end/simproute/goruntime"
-	logger "github.com/he-end/simproute/route_logger"
 )
 
 func (r *Router) Use(mw func(http.Handler) http.Handler) {
@@ -13,13 +10,4 @@ func (r *Router) Use(mw func(http.Handler) http.Handler) {
 	r.MU.Unlock()
 }
 
-func mwAutoCorelation() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			idCorelate := goruntime.GetCorelationID()
-			w.Header().Set("X-Set-Corelation-ID", idCorelate.String())
-			logger.NewLoggerOnRuntime(logger.RegisterRuntime{Key: "request_id", Value: idCorelate.String()})
-			next.ServeHTTP(w, r)
-		})
-	}
-}
+
